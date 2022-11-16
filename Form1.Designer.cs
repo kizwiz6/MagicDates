4abe1ffc53240aece8a3e632e8d986935b8f6fbd
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-16 10:39:01 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace MagicDates
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_day = new System.Windows.Forms.Label();
+            this.lbl_month = new System.Windows.Forms.Label();
+            this.lbl_sum = new System.Windows.Forms.Label();
+            this.lbl_year = new System.Windows.Forms.Label();
+            this.lbl_magic = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(39, 35);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose a Date:";
+            // 
+            // lbl_day
+            // 
+            this.lbl_day.AutoSize = true;
+            this.lbl_day.Location = new System.Drawing.Point(280, 14);
+            this.lbl_day.Name = "lbl_day";
+            this.lbl_day.Size = new System.Drawing.Size(30, 15);
+            this.lbl_day.TabIndex = 2;
+            this.lbl_day.Text = "Day:";
+            // 
+            // lbl_month
+            // 
+            this.lbl_month.AutoSize = true;
+            this.lbl_month.Location = new System.Drawing.Point(408, 15);
+            this.lbl_month.Name = "lbl_month";
+            this.lbl_month.Size = new System.Drawing.Size(46, 15);
+            this.lbl_month.TabIndex = 3;
+            this.lbl_month.Text = "Month:";
+            // 
+            // lbl_sum
+            // 
+            this.lbl_sum.AutoSize = true;
+            this.lbl_sum.Location = new System.Drawing.Point(574, 11);
+            this.lbl_sum.Name = "lbl_sum";
+            this.lbl_sum.Size = new System.Drawing.Size(34, 15);
+            this.lbl_sum.TabIndex = 4;
+            this.lbl_sum.Text = "Sum:";
+            // 
+            // lbl_year
+            // 
+            this.lbl_year.AutoSize = true;
+            this.lbl_year.Location = new System.Drawing.Point(567, 29);
+            this.lbl_year.Name = "lbl_year";
+            this.lbl_year.Size = new System.Drawing.Size(32, 15);
+            this.lbl_year.TabIndex = 5;
+            this.lbl_year.Text = "Year:";
+            // 
+            // lbl_magic
+            // 
+            this.lbl_magic.AutoSize = true;
+            this.lbl_magic.Location = new System.Drawing.Point(294, 44);
+            this.lbl_magic.Name = "lbl_magic";
+            this.lbl_magic.Size = new System.Drawing.Size(134, 15);
+            this.lbl_magic.TabIndex = 6;
+            this.lbl_magic.Text = "Nothing special about it";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 143);
+            this.Controls.Add(this.lbl_magic);
+            this.Controls.Add(this.lbl_year);
+            this.Controls.Add(this.lbl_sum);
+            this.Controls.Add(this.lbl_month);
+            this.Controls.Add(this.lbl_day);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Magic Dates";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private Label lbl_day;
+        private Label lbl_month;
+        private Label lbl_sum;
+        private Label lbl_year;
+        private Label lbl_magic;
+    }
+}
