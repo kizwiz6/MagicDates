189bd85b6680092f9490d31afa7b9642fee6c51d
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-16 10:43:55 +0000

    Creates new integers for date time picker based on Microsoft DateTime Struct (an object with a bunch of properties we can access).

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,6 +43,7 @@
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
             this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // label1
             // 
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+// https://learn.microsoft.com/en-us/dotnet/api/system.datetime?view=net-7.0
+
 namespace MagicDates
 {
     public partial class Form1 : Form
@@ -5,6 +7,16 @@ namespace MagicDates
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime chosenDate = dateTimePicker1.Value;
+
+            int day = chosenDate.Day;
+            int month = chosenDate.Month;
+
+            int year = chosenDate.Year;
         }
     }
 }
