afa61f0d8b5e1c749556f12b38a44a77ff75763f
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-20 18:00:03 +0000

    End of application build that demonstrates how to use date time.

--- Form1.cs
+++ Form1.cs
@@ -24,6 +24,24 @@ namespace MagicDates
 
             int finalTwoValue = int.Parse(lastTwoDigits); // gets final two digits and puts value back into int
 
+            // Check if it is magic number
+
+            if (day + month == finalTwoValue)
+            {
+                lbl_magic.Text = "Magic number!";
+                lbl_magic.ForeColor = Color.Red;
+            }
+            else
+            {
+                lbl_magic.Text = "This looks like an ordinary date.";
+                lbl_magic.ForeColor = Color.Black;
+            }
+
+            // Update the other labels
+            lbl_day.Text = "Day: " + day;
+            lbl_month.Text = "Month: " + month;
+            lbl_year.Text = "Year: " + finalTwoValue;
+            lbl_sum.Text = "Sum: " + (day + month);
         }
     }
 }
